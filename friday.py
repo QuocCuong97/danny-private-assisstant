90e8add796ddae37d4bdf5bcf749c4d632589e00
Ngo Quoc Cuong <49702138+QuocCuong97@users.noreply.github.com>
2021-06-19 23:02:26 +0700

    Update friday.py

--- friday.py
+++ friday.py
@@ -50,7 +50,7 @@ def stop():
 
 def get_text():
     time.sleep(3)
-    for i in range(3):
+    for i in range(100000000000000000000000000000000000000000000000000000000000000000000000000000000000):
         text = get_audio()
         if text:
             return text.lower()
@@ -245,48 +245,49 @@ def help_me():
     10. Kể bạn biết về thế giới """)
 
 def assistant():
-    speak("Xin chào, bạn tên là gì nhỉ?")
-    name = get_text()
-    if name:
+    name = "Cường"
+    # speak("Chào sếp, bạn tên là gì nhỉ?")
+    # name = get_text()
+    # if name:
         # print(name)
-        speak("Chào bạn {}".format(name))
-        time.sleep(1)
-        speak("Bạn cần Friday giúp gì ạ?")
-        while True:
-            text = get_text()
-            if not text:
-                break
-            elif "dừng" in text or "tạm biệt" in text or "chào robot" in text or "ngủ thôi" in text:
-                stop()
-                break
-            elif "có thể làm gì" in text:
-                help_me()
-            elif "chào trợ lý ảo" in text:
-                hello(name)
-            elif "hiện tại" in text:
-                get_time(text)
-            elif "mở" in text:
-                if 'mở google và tìm kiếm' in text:
-                    open_google_and_search(text)
-                elif "." in text:
-                    open_website(text)
-                else:
-                    open_application(text)
-            elif "email" in text or "mail" in text or "gmail" in text:
-                send_email(text)
-            elif "thời tiết" in text:
-                current_weather()
-            elif "chơi nhạc" in text:
-                play_song()
-            elif "hình nền" in text:
-                change_wallpaper()
-            elif "đọc báo" in text:
-                read_news()
-            elif "định nghĩa" in text:
-                tell_me_about()
+        # speak("Chào bạn {}".format(name))
+        # time.sleep(1)
+    speak("Cường cần Friday giúp gì nào?")
+    while True:
+        text = get_text()
+        if not text:
+            break
+        elif "dừng" in text or "tạm biệt" in text or "chào robot" in text or "ngủ thôi" in text:
+            stop()
+            break
+        elif "có thể làm gì" in text:
+            help_me()
+        elif "chào trợ lý ảo" in text:
+            hello(name)
+        elif "hiện tại" in text:
+            get_time(text)
+        elif "mở" in text:
+            if 'mở google và tìm kiếm' in text:
+                open_google_and_search(text)
+            elif "." in text:
+                open_website(text)
             else:
-                pass
-                # speak("Bạn cần Bot giúp gì ạ?")
+                open_application(text)
+        elif "email" in text or "mail" in text or "gmail" in text:
+            send_email(text)
+        elif "thời tiết" in text:
+            current_weather()
+        elif "chơi nhạc" in text:
+            play_song()
+        elif "hình nền" in text:
+            change_wallpaper()
+        elif "đọc báo" in text:
+            read_news()
+        elif "định nghĩa" in text:
+            tell_me_about()
+        else:
+            pass
+            # speak("Bạn cần Bot giúp gì ạ?")
 
 assistant()
 
